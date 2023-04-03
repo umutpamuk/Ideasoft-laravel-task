1784a4871355b42352c2aaf30537ac3dcba8ad02
umutpamuk <umutpamuk59@gmail.com>
2023-04-03 22:08:30 +0300

    Tablo tasarımı yapıldı.

--- database/migrations/2023_04_03_130019_create_orders_table.php
+++ database/migrations/2023_04_03_130019_create_orders_table.php
@@ -14,19 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('customer_id');
-            $table->foreign('customer_id')->references('id')->on('customers');
-            $table->decimal('total', 8, 2);
-            $table->timestamps();
-        });
-
-        Schema::create('order_items', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('order_id');
-            $table->foreign('order_id')->references('id')->on('orders');
-            $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->unsignedInteger('quantity');
-            $table->decimal('unit_price', 8, 2);
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');;
             $table->decimal('total', 8, 2);
             $table->timestamps();
         });
@@ -37,7 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('order_items');
         Schema::dropIfExists('orders');
     }
 };
