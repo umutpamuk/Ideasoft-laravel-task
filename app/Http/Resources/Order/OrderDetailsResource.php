1fb24eb70ecbf3012c52171b320cd134aedb4d22
umutpamuk <umutpamuk59@gmail.com>
2023-04-03 22:16:20 +0300

    OrderDetails ve OrderItemDetails için response şekli eklendi.

---
+++ app/Http/Resources/Order/OrderDetailsResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Order;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderDetailsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'customerId' => $this->customer_id,
+            'items' => new OrderItemDetailsCollection($this['items']),
+            'total' => $this->total,
+        ];
+    }
+}
